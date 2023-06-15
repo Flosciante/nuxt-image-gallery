1b463e93545d42e8ec0bcfc7ad89eb23657dd892
Florent Delerue <florentdelerue@hotmail.com>
2023-06-15 17:53:03 +0200

    refactor: use uicon on drag zone

--- components/UploadButton.vue
+++ components/UploadButton.vue
@@ -7,15 +7,15 @@ const isSmallScreen = useMediaQuery('(max-width: 1024px)')
 <template>
   <UButton variant="outline" color="white" class="border border-1 border-gray-500 border-dashed ring-transparent h-[100px] md:h-[200px] lg:h-[430px] transition-colors duration-200 rounded-md upload group w-full" :rounded="false">
     <div v-if="!isSmallScreen" class="w-full rounded-md flex items-center justify-center h-[430px]">
-      <img src="/icons/upload.svg" class="group-hover:hidden h-12 w-12 m-auto absolute" />
+      <UIcon name="i-heroicons-arrow-up-tray" class="group-hover:hidden h-12 w-12 m-auto absolute" />
       <div class="relative opacity-0 group-hover:opacity-100 flex w-full h-full justify-center items-center transition-all duration-100">
-        <img src="/icons/upload-solid.svg" class="absolute m-auto h-12 w-12 transition-all duration-100 group-hover:-translate-y-5" />
+        <UIcon name="i-heroicons-arrow-up-tray" class="absolute m-auto h-12 w-12 transition-all duration-100 group-hover:-translate-y-5" />
         <span class="absolute m-auto group-hover:translate-y-5 transition-all duration-100 ">Drag & drop to upload</span>
       </div>
     </div>
     <div v-else class="w-full rounded-md flex items-center justify-center h-[100px] md:h-[200px]">
       <div class="relative flex flex-col w-full h-full justify-center items-center">
-        <img src="/icons/upload-solid.svg" class="h-12 w-12" />
+        <UIcon name="i-heroicons-arrow-up-tray" class="w-12 h-12" />
         <span>Press to upload</span>
       </div>
     </div>
