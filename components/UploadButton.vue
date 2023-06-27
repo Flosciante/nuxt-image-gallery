0d20d97d0be1744376ab631d1b42adde68e3a412
Florent Delerue <florentdelerue@hotmail.com>
2023-06-27 15:12:42 +0200

    refactor(uploadbutton): force white text

--- components/UploadButton.vue
+++ components/UploadButton.vue
@@ -10,13 +10,13 @@ const isSmallScreen = useMediaQuery('(max-width: 1024px)')
       <UIcon name="i-heroicons-arrow-up-tray" class="group-hover:hidden h-12 w-12 m-auto absolute" />
       <div class="relative opacity-0 group-hover:opacity-100 flex w-full h-full justify-center items-center transition-all duration-100">
         <UIcon name="i-heroicons-arrow-up-tray" class="absolute m-auto h-12 w-12 transition-all duration-100 group-hover:-translate-y-5" />
-        <span class="absolute m-auto opacity-0 group-hover:opacity-100 group-hover:translate-y-5 transition-all duration-200 ">Click or Drag & drop to upload</span>
+        <span class="absolute m-auto opacity-0 group-hover:opacity-100 group-hover:translate-y-5 transition-all duration-200 text-white">Click or Drag & drop to upload</span>
       </div>
     </div>
     <div v-else class="w-full rounded-md flex items-center justify-center h-[100px] md:h-[200px]">
       <div class="relative flex flex-col w-full h-full justify-center items-center">
-        <UIcon name="i-heroicons-arrow-up-tray" class="w-12 h-12" />
-        <span>Press to upload</span>
+        <UIcon name="i-heroicons-arrow-up-tray" class="w-12 h-12 text-white" />
+        <span class="text-white">Press to upload</span>
       </div>
     </div>
   </UButton>
