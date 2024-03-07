14f33ccc269011888fe5b18f30401815d0dc57f7
Sébastien Chopin <seb@nuxt.com>
2024-03-07 17:14:49 +0100

    chore: simplify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxthub/core', '@nuxt/ui-pro'],
-
   modules: [
+    '@nuxt/fonts',
     '@nuxt/ui',
-    '@nuxtjs/fontaine',
-    '@nuxtjs/google-fonts',
     '@vueuse/nuxt',
   ],
-
-  fontMetrics: {
-    fonts: ['DM Sans'],
-  },
-
-  googleFonts: {
-    display: 'swap',
-    download: true,
-    families: {
-      'DM+Sans': [400, 500, 600, 700],
-    },
-  },
-
   devtools: { enabled: true },
 })
--- server/routes/images/[pathname].get.ts
+++ server/routes/images/[pathname].get.ts
@@ -1,5 +1,9 @@
+import { z } from 'zod'
+
 export default eventHandler(async (event) => {
-  const { pathname } = event.context.params || {}
+  const { pathname } = await getValidatedRouterParams(event, z.object({
+    pathname: z.string().min(1),
+  }).parse)
 
   return hubBlob().serve(event, pathname)
 })
