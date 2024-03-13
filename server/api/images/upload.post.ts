162c0ba93b9c74fc56e15c9c950ffd65dd4ce0ee
Florent Delerue <florentdelerue@hotmail.com>
2024-03-13 10:37:17 +0100

    fix: allowed jpg upload

--- server/api/images/upload.post.ts
+++ server/api/images/upload.post.ts
@@ -2,7 +2,8 @@ export default eventHandler(async (event) => {
   const form = await readFormData(event)
   const image = form.get('image') as File
 
-  ensureBlob(image, { maxSize: '8MB', types: ['image/jpeg', 'image/png', 'image/gif', 'image/heic', 'image/webp'] })
+  // @ts-expect-error: jpg format
+  ensureBlob(image, { maxSize: '8MB', types: ['image/jpeg', 'image/png', 'image/gif', 'image/heic', 'image/webp', 'image/jpg'] })
 
   return hubBlob().put(image.name, image)
 })
