1ac84dc7e9d4ca4f7d772479ca0227059d25ee0b
Florent Delerue <florentdelerue@hotmail.com>
2024-03-19 16:57:17 +0100

    chore: remove ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxthub/core', '@nuxt/ui-pro'],
+  extends: ['@nuxthub/core'],
 
   experimental: {
     viewTransition: true,
