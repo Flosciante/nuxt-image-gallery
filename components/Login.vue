fb7b1599e7835f743ce1c305490b029f44be00d0
Sébastien Chopin <seb@nuxt.com>
2024-04-03 18:50:46 +0200

    fix: ux with password
    resolves #10

--- components/Login.vue
+++ components/Login.vue
@@ -7,25 +7,24 @@ const loading = ref(false)
 const toast = useToast()
 
 async function login() {
+  if (loading.value || !password.value) return
   loading.value = true
-
-  if (password.value) {
-    await $fetch('/api/auth', {
-      method: 'POST',
-      body: { password: password.value },
+  await $fetch('/api/auth', {
+    method: 'POST',
+    body: { password: password.value },
+  })
+    .then(async () => {
+      await refreshSession()
+      emit('closeLogin')
     })
-      .then(async () => {
-        await refreshSession()
-        emit('closeLogin')
+    .catch((err) => {
+      toast.add({
+        title: `Error ${err.statusCode}`,
+        description: `${err.data?.message || err.message}. Please try again`,
+        color: 'red',
       })
-      .catch((err) => {
-        toast.add({
-          title: `Error ${err.statusCode}`,
-          description: `${err.data?.message || err.message}. Please try again`,
-          color: 'red',
-        })
-      }).finally(() => loading.value = false)
-  }
+    })
+  loading.value = false
 }
 </script>
 
@@ -36,6 +35,6 @@ async function login() {
     </h1>
     <UInput v-model="password" type="password" placeholder="Enter password" icon="i-heroicons-key" class="!w-60" />
 
-    <UButton :loading="loading" type="submit" label="Login" color="green" variant="ghost" class="px-4" size="lg" />
+    <UButton :loading="loading" type="submit" label="Login" color="green" variant="ghost" class="px-4" size="lg" :disabled="!password" />
   </form>
 </template>
