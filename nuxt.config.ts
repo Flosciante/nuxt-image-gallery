7a9acca46e8981ad503dc80c1eac48b2d5f352ee
Sébastien Chopin <seb@nuxt.com>
2024-06-11 17:24:19 +0200

    chore: update to nuxt 4 compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: { compatibilityVersion: 4 },
+
   modules: [
     '@nuxthub/core',
     '@nuxt/fonts',
